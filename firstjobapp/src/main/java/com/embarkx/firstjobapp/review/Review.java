e101ceba8cb7ad682136f59659a9dd60299bf080
Prajwal M V <mvprajwal546@gmail.com>
2025-08-30 09:37:35 +0530

    Review Created succesfully

--- firstjobapp/src/main/java/com/embarkx/firstjobapp/review/Review.java
+++ firstjobapp/src/main/java/com/embarkx/firstjobapp/review/Review.java
@@ -1,4 +1,56 @@
 package com.embarkx.firstjobapp.review;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double rating;
+
+    public Review(Long id, String title, String description, double rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+    public Review() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
